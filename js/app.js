44433fa1da151756f0977730dc571ec4a2b1cc5a
YoannMis <ymisericordia@gmx.com>
2025-10-15 11:30:47 +0200

    Step2 completed

--- js/app.js
+++ js/app.js
@@ -10,9 +10,23 @@ for (let round = 0; round < userInput; round++) {
   // On crée un élément pixel
   const pixel = document.createElement("div");
   // On lui ajoute la class="pixel"
-  pixel.classList.add("pixel");
+  pixel.classList.add("pixel", "grey");
   // On l'ajoute dans la #grid
   grid.appendChild(pixel);
   // 
   }
+}
+
+const pixels = document.querySelectorAll(".pixel");
+
+for (const pixel of pixels){
+  // Écouter l'évènement 'click' sur un pixel
+  // Au clic on change la couleur du pixel avec un changement de classe
+  pixel.addEventListener("click", () => {
+    if (pixel.classList.contains("grey")) {
+    pixel.classList.replace("grey", "dark");
+  } else if (pixel.classList.contains("dark")){
+    pixel.classList.replace("dark", "grey");
+  }
+  });
 }
