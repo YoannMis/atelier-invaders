fad944b3a04a8a402369c3265f2d8e4e14052c9d
YoannMis <ymisericordia@gmx.com>
2025-10-15 10:39:01 +0200

    Step1 completed

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-// ton code ici
+// On récupère l'objet #grid
+const grid = document.getElementById("grid");
+// Demande à l'utilisateur de la taille de la grille
+const userInput = Number(prompt("Taille de la grille ?"));
+// On met un style CSS à la grille pour gérer le nombre de colonnes
+grid.style.gridTemplateColumns = `repeat(${userInput}, 1fr)`;
+
+for (let round = 0; round < userInput; round++) {
+  for (let round = 0; round < userInput; round++) {
+  // On crée un élément pixel
+  const pixel = document.createElement("div");
+  // On lui ajoute la class="pixel"
+  pixel.classList.add("pixel");
+  // On l'ajoute dans la #grid
+  grid.appendChild(pixel);
+  // 
+  }
+}
